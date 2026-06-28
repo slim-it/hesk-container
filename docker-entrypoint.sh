a7ef799e26a6034fa2e23b305b571e6a65393a32
Lex Brugman <lex@probaa.nl>
2026-06-28 21:47:59 +0200

    Gate installer exposure

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,14 @@ if [ ! -e /data/hesk_settings.inc.php ]; then
   cp /usr/local/share/hesk-defaults/hesk_settings.inc.php /data/hesk_settings.inc.php
 fi
 
+if [ "${HESK_ENABLE_INSTALL:-0}" = "1" ]; then
+  rm -rf /var/www/html/install
+  cp -a /usr/local/share/hesk-install /var/www/html/install
+  chown -R www-data:www-data /var/www/html/install
+else
+  rm -rf /var/www/html/install
+fi
+
 chown -R www-data:www-data /data
 chmod 0640 /data/hesk_settings.inc.php || true
 
